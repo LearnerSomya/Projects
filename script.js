18a4be9ac2c680db29cd48f334574b2d1fa9bc33
LearnerSomya <somyasinghal71001@gmail.com>
2023-11-23 22:31:14 +0530

    First Page Animation

--- script.js
+++ script.js
@@ -2,6 +2,33 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+//Navigation bar Animation
+function firstPageAnimation(){
+    var tl = gsap.timeline(); 
+
+    tl.from("#nav",{
+        y: '-10',
+        opacity:0,
+        duration: 1.5,
+        ease: Expo.easeInOut,
+    })
+
+    .to(".boundingelem",{
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 2,
+        delay: -1,
+        stagger: .2
+    })
+    .from("herofooter",{
+        y: -10,
+        opacity: 0,
+        duration: 1.5,
+        delay: -1,
+        ease: Expo.easeInOut
+    })
+}
+firstPageAnimation();
 // Mouse Circle 
 function MouseCircleFollow(){
     //here mousemove is inbuilt control which refelects movement of mouse
