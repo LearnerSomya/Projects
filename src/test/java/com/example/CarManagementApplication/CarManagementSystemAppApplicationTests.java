8addb0230e858f6564faf92cd8f46fb9ad0eb26b
LearnerSomya <somyasinghal71001@gmail.com>
2024-11-16 23:33:16 +0530

    All working files that are the functinality of the App

---
+++ src/test/java/com/example/CarManagementApplication/CarManagementSystemAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarManagementApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarManagementSystemAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
