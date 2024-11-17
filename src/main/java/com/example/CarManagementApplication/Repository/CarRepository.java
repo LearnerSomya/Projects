3fbe66b2826d8490e35fcdaa119c69bd4e1335e0
LearnerSomya <somyasinghal71001@gmail.com>
2024-11-17 19:14:44 +0530

    Managed Repo

--- src/main/java/com/example/CarManagementApplication/Repository/CarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.CarManagementApplication.Repository;
-
-import com.example.CarManagementApplication.Model.CarDetails;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CarRepository extends MongoRepository<CarDetails, String> {
-}
